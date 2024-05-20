e9226c5520abdb04264c912efe25535a2c520094
hashrock <hashrock@users.noreply.github.com>
2024-05-20 13:29:48 +0900

    bi-direction linked list

--- main.ts
+++ main.ts
@@ -1,10 +1,19 @@
 export class Node {
   data: number;
   next: Node | null = null;
+  prev: Node | null = null;
 
   constructor(data: number) {
     this.data = data;
   }
+}
+
+export function insert(node: Node, data: number) {
+  // TODO
+}
+
+export function remove(node: Node) {
+  // TODO
 }
 
 export function appendTail(node: Node, data: number) {
@@ -14,4 +23,5 @@ export function appendTail(node: Node, data: number) {
     n = n.next;
   }
   n.next = end;
+  end.prev = n;
 }
--- main_test.ts
+++ main_test.ts
@@ -1,12 +1,21 @@
 import { assertEquals } from "jsr:@std/assert";
 import { appendTail, Node } from "./main.ts";
 
-Deno.test(function appendTest() {
+function setup() {
   const root = new Node(1);
-  root.next = new Node(2);
+  appendTail(root, 2);
   appendTail(root, 3);
+  return root;
+}
 
+Deno.test(function appendTest() {
+  const root = setup();
   assertEquals(root.data, 1);
-  assertEquals(root.next.data, 2);
+  assertEquals(root.next?.data, 2);
+  assertEquals(root.next?.prev?.data, 1);
   assertEquals(root.next?.next?.data, 3);
+});
+
+Deno.test(function insertTest() {
+  const root = setup();
 });
