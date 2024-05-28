2c0a77959b0fd6ec9ff29b8c9a6f1ab74e3359db
hashrock <hashrock@users.noreply.github.com>
2024-05-28 20:55:53 +0900

    add clonelist

--- 04-sorted-list-2/list.ts
+++ 04-sorted-list-2/list.ts
@@ -60,3 +60,24 @@ export function appendTail(node: ListNode, data: number) {
   }
   n.next = end;
 }
+
+export function cloneList(head: ListNode | null): ListNode | null {
+  let readCur = head;
+  let output = null;
+  let writePrev: ListNode | null = null;
+
+  while (readCur) {
+    const node = new ListNode(readCur.val);
+    if (output === null) {
+      output = node;
+    } else {
+      if (writePrev !== null) {
+        writePrev.next = node;
+      }
+    }
+    writePrev = node;
+    readCur = readCur.next;
+  }
+
+  return output;
+}
